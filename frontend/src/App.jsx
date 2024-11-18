9ada399114610f4326f2160d121230a207cfcae3
ismail01-jl <jellitiismail2002@gmail.com>
2024-11-18 20:49:00 +0100

    Create Pages and Routes (frontend)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signin from './pages/Signin.jsx';
+import About from './pages/AboutPage.jsx';
+import SignUp from './pages/Signup.jsx'
+import Profile from './pages/Profile.jsx'
+import Home from './pages/Home.jsx'
 
 export default function App() {
   return (
-    <h1 className='text'>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
